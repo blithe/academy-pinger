f2122cba13136fdbacb0cdbd0748ff4bf8f0272d
blithe <blitherocher@gmail.com>
2021-01-19 21:19:46 -0800

    fix creds

--- academy.rb
+++ academy.rb
@@ -45,7 +45,7 @@ def send_email(content)
 	content = Content.new(type: 'text/plain', value: content)
 	mail = Mail.new(from, subject, to, content)
 
-	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+	sg = SendGrid::API.new(user_name: ENV['SENDGRID_USERNAME'], password: ENV['SENDGRID_PASSWORD'])
 	response = sg.client.mail._('send').post(request_body: mail.to_json)
 	puts response.status_code
 	puts response.body
