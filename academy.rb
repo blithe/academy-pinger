c12fa714d97b3e711e11dd2299cff064940aedba
blithe <blitherocher@gmail.com>
2021-01-19 21:15:48 -0800

    add sendgrid

--- academy.rb
+++ academy.rb
@@ -1,6 +1,9 @@
 require 'json'
 require 'uri'
 require 'net/http'
+require 'sendgrid-ruby'
+include SendGrid
+
 
 def run
 	puts 'starting'
@@ -34,4 +37,19 @@ def run
 	puts 'done'
 end
 
-run
+def send_email(content)
+
+	from = Email.new(email: 'test@example.com')
+	subject = 'Hello World from the SendGrid Ruby Library!'
+	to = Email.new(email: 'blitherocher@gmail.com')
+	content = Content.new(type: 'text/plain', value: content)
+	mail = Mail.new(from, subject, to, content)
+
+	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	puts response.status_code
+	puts response.body
+	puts response.headers
+end
+
+send_email(run)
