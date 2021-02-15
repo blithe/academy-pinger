249ea87f3763c316fa76c1e0dee668f00d922127
blithe <blitherocher@gmail.com>
2021-02-15 10:02:38 -0800

    add working cvs implementation (without email)

--- vaccine.rb
+++ vaccine.rb
@@ -16,7 +16,7 @@ def heb_results
 	stores = info.dig('locations')
 
 
-	output = "Found #{stores.count} stores"
+	output = "Found #{stores.count} HEB stores"
 
 	ip = IPSocket.getaddress(Socket.gethostname)
 	output << "\nIP: #{ip}"
@@ -43,6 +43,41 @@ def heb_results
 	output
 end
 
+def cvs_results
+	radius = 500
+
+	uri = URI("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.TX.json?vaccineinfo")
+
+	response = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+		request = Net::HTTP::Get.new uri
+		request['Referer'] = 'https://www.cvs.com/immunizations/covid-19-vaccine?icid=cvs-home-hero1-banner-1-link2-coronavirus-vaccine'
+
+		http.request request # Net::HTTPResponse object
+	end
+
+	info = JSON.parse(response.body)
+
+	stores = info.dig('responsePayloadData', 'data', 'TX')
+
+	output = "Found #{stores.count} CVS stores"
+
+	ip = IPSocket.getaddress(Socket.gethostname)
+	output << "\nIP: #{ip}"
+
+	stores.each do |store|
+
+	# require 'pry'; binding.pry
+		status = store['status']
+		next if status == 'Fully Booked'
+
+		city = store['city']
+
+		output << ("\n#{city} has availability")
+	end
+
+	output
+end
+
 def send_email(content)
 
 	api_key = ENV['MAILGUN_API_KEY']
@@ -58,6 +93,7 @@ end
 
 puts 'starting'
 puts heb_results
+puts cvs_results
 
-send_email(heb_results)
+# send_email(heb_results)
 puts 'done'
